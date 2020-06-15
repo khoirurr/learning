bdc4e8234217baaeb1f3430b90bcf03775de89df
khoirurr <rozakkhoirur@gmail.com>
2020-06-15 23:36:53 +0700

    ubah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel learning
+                    khoirur
                 </div>
 
                 <div class="links">
