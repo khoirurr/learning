52bd6f21b7ff7ef5bd300b20753e4b8a6b3787a6
khoirurr <rozakkhoirur@gmail.com>
2020-06-15 23:08:33 +0700

    perubahan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel learning
                 </div>
 
                 <div class="links">
