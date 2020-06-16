841414565121f5222970b6793b1777f4c63b2ba7
khoirurr <rozakkhoirur@gmail.com>
2020-06-16 23:29:55 +0700

    namapanjang

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    khoirur
+                    khoirur rozak
+                    
                 </div>
 
                 <div class="links">
